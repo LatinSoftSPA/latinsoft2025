cac0bfa0354f2971121d03db03192c16b63aba1e
mveraalbarenque <don.marcos.vera@gmail.com>
2025-01-01 21:52:35 -0300

    fix: aling content Prestations Components

---
+++ src/App/Main/components/Prestations/data.ts
@@ -0,0 +1,25 @@
+import { PrestationProps } from "./interfaces";
+
+export const listPrestations: PrestationProps[] = [
+  {
+    id: 1,
+    name: "GesTRA",
+    img: "img/ilustracion8.svg",
+    description: "Sistema de Gestión de Transporte Publico Mayor.",
+    enabled: true,
+  },
+  {
+    id: 2,
+    name: "SACGes",
+    img: "img/ilustracion7.svg",
+    description: "Sistema de Acompañamiento para la Gestión Directiva.",
+    enabled: true,
+  },
+  {
+    id: 3,
+    name: "GesFLO",
+    img: "img/ilustracion4.svg",
+    description: "Sistema de Gestión de Flotas para los servicios mineros.",
+    enabled: true,
+  },
+];
--- src/App/Main/components/Prestations/index.tsx
+++ src/App/Main/components/Prestations/index.tsx
@@ -1,36 +1,25 @@
+import Prestation from "./Prestation";
+
+import { PrestationProps } from "./interfaces";
+import { listPrestations } from "./data";
+
 import style from "./index.module.css";
 
-interface PrestationsProps {
+interface Props {
   id: string;
 }
 
-const Prestations: React.FC<PrestationsProps> = ({ id }) => {
+const Prestations: React.FC<Props> = ({ id }) => {
+  const filtered = listPrestations.filter((obj: PrestationProps) => obj.enabled);
+
   return (
     <div className={style.container}>
       <section id={id}>
         <h2 className={style.title}>Nuestros servicios</h2>
         <div className={style.prestations}>
-          <div className={style.prestation}>
-            <figure>
-              <img src="img/ilustracion8.svg" alt="" />
-            </figure>
-            <h3>GesTRA</h3>
-            <p>Sistema de Gestión de Transporte Publico Mayor.</p>
-          </div>
-          <div className={style.prestation}>
-            <figure>
-              <img src="img/ilustracion7.svg" alt="" />
-            </figure>
-            <h3>SACGes</h3>
-            <p>Sistema de Acompañamiento para la Gestión Directiva.</p>
-          </div>
-          <div className={style.prestation}>
-            <figure>
-              <img src="img/ilustracion4.svg" alt="" />
-            </figure>
-            <h3>GesFLO</h3>
-            <p>Sistema de Gestión de Flotas para los servicios mineros.</p>
-          </div>
+          {filtered.map((obj: PrestationProps) => (
+            <Prestation key={obj.id} {...obj} />
+          ))}
         </div>
       </section>
     </div>
